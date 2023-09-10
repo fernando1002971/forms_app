f3e546780eb2b75e45e6e1a1652b8764bbb82a36
fernando1002971 <fernando1002971@gmail.com>
2023-09-09 21:34:38 -0600

    Primer commit

---
+++ lib/presentation/blocs/counter_cubit/counter_cubit.dart
@@ -0,0 +1,25 @@
+import "package:flutter_bloc/flutter_bloc.dart";
+
+part 'counter_state.dart';
+
+//CounterCubit tiene acceso al state o estado
+class CounterCubit extends Cubit<CounterState> {
+  CounterCubit() : super(CounterState(counter: 5));
+
+//Métodos que emiten nuevos estados
+
+//1. Función que permite incrementar
+  void increaseBy(int value) {
+    emit(state.copyWith(
+        counter: state.counter + value,
+        transactionCount: state.transactionCount + 1));
+  }
+
+//2- Función que permita reiniciar el conteo
+  void reset() {
+    emit(state.copyWith(
+        counter: 0,
+    )
+      );
+  }
+}
